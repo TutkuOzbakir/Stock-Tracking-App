f153c1c9cbd74241408d817e05a8f554635e5f6d
Tutku Özbakır <tutkuozbakir528@gmail.com>
2024-03-12 20:18:02 +0300

    Removed duplicate values

--- app/src/main/java/com/tutkuozbakir/stocktracking/model/StockItem.kt
+++ app/src/main/java/com/tutkuozbakir/stocktracking/model/StockItem.kt
@@ -2,9 +2,10 @@ package com.tutkuozbakir.stocktracking.model
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "items")
+@Entity(tableName = "items", indices = [Index("item_name", unique = true)])
 data class StockItem(
     @ColumnInfo("item_name")
     var item_name: String,
